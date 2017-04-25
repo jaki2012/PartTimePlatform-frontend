b8efadcf53dd98a6a6f05087a45df3b7413c0c83
jaki2012 <jaki2012@sina.cn>
2017-04-25 18:05:04 +0800

    微调网页主体字体

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Hiragino Sans GB', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
