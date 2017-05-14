7744b95acec2e331d64d9303a9eed41fac7a9dbf
jaki2012 <jaki2012@sina.cn>
2017-05-14 08:25:16 +0800

    将sessionStorage换成localStorage 完成首页强制渲染

--- src/vuex/store/user.js
+++ src/vuex/store/user.js
@@ -4,14 +4,14 @@ export const USER_SIGNIN = "USER_SIGNIN";
 export const USER_SIGNOUT = "USER_SIGNOUT";
 
 export default {
-    state: JSON.parse(sessionStorage.getItem('user')) || {},
+    state: JSON.parse(localStorage.getItem('user')) || {},
     mutations: {
         [USER_SIGNIN](state, user) {
-            sessionStorage.setItem('user', JSON.stringify(user))
+            localStorage.setItem('user', JSON.stringify(user))
             Object.assign(state, user)
         },
         [USER_SIGNOUT](state){
-            sessionStorage.removeItem('user')
+            localStorage.removeItem('user')
             Object.keys(state).forEach(k => Vue.delete(state,k))
         }
     },
